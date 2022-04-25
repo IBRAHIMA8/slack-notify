b98a775e18fa6cf852d8ca2f488ccb4af196f707
IBRAHIMA <hadi.ibrahima8@gmail.com>
2022-04-25 03:41:56 +0100

    Some parameters

--- sample.rb
+++ sample.rb
@@ -1,6 +1,6 @@
 require "slack-notify"
 require 'clockwork'
-sample = SlackNotify::Client.new(webhook_url: 'https://hooks.slack.com/services/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')
+sample = SlackNotify::Client.new(webhook_url: 'SLACK_TOKEN')
 
 include Clockwork
 
